b62bcac1f182ec123a41c4c9e2e110359d64fae4
chae-munho <ansgh4300@naver.com>
2026-03-31 13:50:51 +0900

    기본 ip주소 수정

--- TrainClient/Models/CameraAlertItem.cs
+++ TrainClient/Models/CameraAlertItem.cs
@@ -2,7 +2,7 @@
 {
     public class CameraAlarmItem
     {
-        public int TrainNo { get; set; }
+        public int TrainNo { get; set; }   //Train 필드와 동일
         public int CarNo { get; set; }
         public string DisplayText => $"[기차{TrainNo}] {CarNo}번 호출";
     }
